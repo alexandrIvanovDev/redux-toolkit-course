9af566b821831b9aa62fc68ed8ee9e8eb8d4cb43
AlexandrIvanov <alexandr.1996@list.ru>
2023-07-11 13:15:53 +0400

    Add recipesApi.ts

---
+++ src/store/api/recipesApi.ts
@@ -0,0 +1,28 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import {RecipeType} from '../favorite/favorite-slice.ts';
+
+
+export const recipesApi = createApi({
+    reducerPath: 'recipeApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+    tagTypes: ['Recipe'],
+    endpoints: (builder) => ({
+        getRecipes: builder.query<Array<RecipeType>, string>({
+            query: () => `/recipes`,
+            providesTags: () => ['Recipe']
+        }),
+        getRecipeById: builder.query<RecipeType, number>({
+            query: (id) => `/recipes/${id}`,
+        }),
+        addRecipe: builder.mutation({
+            query: (title) => ({
+              url: '/recipes',
+              method: 'POST',
+              body: title
+            }),
+            invalidatesTags: ['Recipe']
+        }),
+    }),
+})
+
+export const {useGetRecipesQuery, useGetRecipeByIdQuery, useAddRecipeMutation} = recipesApi
