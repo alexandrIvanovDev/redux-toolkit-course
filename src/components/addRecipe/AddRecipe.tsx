6bb386a379cc90282f21c3f74a6ad6e48d7550f6
AlexandrIvanov <alexandr.1996@list.ru>
2023-07-12 21:55:36 +0400

    Refactoring AddRecipe

--- src/components/addRecipe/AddRecipe.tsx
+++ src/components/addRecipe/AddRecipe.tsx
@@ -1,4 +1,4 @@
-import {FormEvent, useState} from 'react';
+import {ChangeEvent, FormEvent, useState} from 'react';
 import {useAddRecipeMutation} from '../../store/api/recipesApi.ts';
 import stylesForButton from '../recipeItem/RecipeItem.module.css'
 import styles from './AddRecipe.module.css'
@@ -10,22 +10,48 @@ export const AddRecipe = () => {
 
     const [title, setTitle] = useState('')
 
+    const [img, setImg] = useState('')
+
+    const [error, setError] = useState<null | string>(null)
+
     const [addRecipe] = useAddRecipeMutation()
 
     const submitForm = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        const recipe: RecipeType = {id: +v1(), title, img: ''}
+        if (title.trim() === '') {
+            setError('Error: the title should not be empty')
+            return
+        }
+        const recipe: RecipeType = {id: +v1(), title, img}
         await addRecipe(recipe)
         setTitle('')
+        setImg('')
+    }
+
+    const onTitleInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        if (error) {
+            setError(null)
+        }
+        setTitle(e.currentTarget.value)
     }
 
     return (
-        <form onSubmit={submitForm}>
+        <form onSubmit={submitForm} className={styles.form}>
+            <label>Create new recipe:</label>
             <input
                 type="text"
                 value={title}
-                onChange={(e) => setTitle(e.currentTarget.value)}
+                onChange={onTitleInputHandler}
                 className={styles.input}
+                placeholder='Title'
+            />
+            {error && <div>{error}</div>}
+            <input
+                type="text"
+                value={img}
+                onChange={(e) => setImg(e.currentTarget.value)}
+                className={styles.input}
+                placeholder='Img'
             />
             <button className={stylesForButton.button} type='submit'>Add recipe</button>
         </form>
